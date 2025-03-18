b3c292a105bcbc26c7591b2cc94c9c2209a10938
mkanke-real <mikekanke@gmail.com>
2025-03-19 01:08:40 +0900

    Boombox Time (#526)
    * Initial Test Commit
    
    * Most features working now.
    
    * Added an attack and backpack sprites
    
    * Renamed so it actually works
    
    * Added in hand sprite inital
    
    * All sprites show now and in an easy to way to add more versions later
    
    * Filling out a bunch of folders for recolors
    
    * Fixed some tempoary RSI issues. Adds a board, lathe recipe, research and the start of the building process
    
    * The boombox now functions thanks Tay and Rainbeon!
    
    * Most stuff implmented. Just a single line bug to fix and add some more sprites and loot tables
    
    * Fixes missing base layer issues, updated textures and added boxes to loot tables
    
    * Made a small change to what you get back from braking the boombox
    
    * fixed lathe yaml spacing
    
    * Change boombox destroy values

--- Content.Shared/Audio/Jukebox/JukeboxComponent.cs
+++ Content.Shared/Audio/Jukebox/JukeboxComponent.cs
@@ -38,11 +38,15 @@ public sealed partial class JukeboxComponent : Component
     [DataField]
     public string? SelectState;
 
+    [DataField]
+    public bool NeedsBattery = false;
+
     [ViewVariables]
     public bool Selecting;
 
     [ViewVariables]
     public float SelectAccumulator;
+
 }
 
 [Serializable, NetSerializable]
