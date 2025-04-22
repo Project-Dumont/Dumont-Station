4be0ae996d5bdbc1d7e434d323cf4562900b7b3f
ss14-Starlight <ss14-Starlight@outlook.com>
2025-04-22 05:37:08 +0200

    Add hitscan cartrige damage examine

--- Content.Server/Weapons/Ranged/Systems/GunSystem.Cartridges.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.Cartridges.cs
@@ -14,12 +14,16 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Shared._Starlight.Weapon.Components;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Events;
 using Content.Shared.Examine;
 using Content.Shared.Projectiles;
+using Content.Shared.Weapons.Ranged;
 using Content.Shared.Weapons.Ranged.Components;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Toolshed.Commands.Values;
+using Robust.Shared.Utility;
 
 namespace Content.Server.Weapons.Ranged.Systems;
 
@@ -31,7 +35,10 @@ public sealed partial class GunSystem
         SubscribeLocalEvent<CartridgeAmmoComponent, ExaminedEvent>(OnCartridgeExamine);
         SubscribeLocalEvent<CartridgeAmmoComponent, DamageExamineEvent>(OnCartridgeDamageExamine);
         SubscribeLocalEvent<BasicEntityAmmoProviderComponent, DamageExamineEvent>(OnBasicEntityDamageExamine); // Goobstation
+        SubscribeLocalEvent<HitScanCartridgeAmmoComponent, ExaminedEvent>(OnHitScanCartridgeExamine);
+        SubscribeLocalEvent<HitScanCartridgeAmmoComponent, DamageExamineEvent>(OnHitScanCartridgeDamageExamine);
     }
+
 
     private void OnCartridgeDamageExamine(EntityUid uid, CartridgeAmmoComponent component, ref DamageExamineEvent args)
     {
@@ -113,4 +120,64 @@ public sealed partial class GunSystem
         return p.IgnoreResistances ? 100 : (int)Math.Round(p.Damage.ArmorPenetration * 100);
     }
     // Goobstation end
+
+    private void OnHitScanCartridgeDamageExamine(EntityUid uid, HitScanCartridgeAmmoComponent component, ref DamageExamineEvent args)
+    {
+        var damageSpec = GetHitscanProjectileDamage(component.Hitscan);
+
+        if (damageSpec == null)
+            return;
+
+        _damageExamine.AddDamageExamine(args.Message, Damageable.ApplyUniversalAllModifiers(damageSpec), Loc.GetString("damage-projectile"));
+
+        // var ArmorMessage = GetArmorPenetrationExplain(component.Hitscan);
+
+        // args.Message.AddMessage(ArmorMessage);
+
+    }
+
+    // TODO. Gaby Station. Portar https://github.com/ss14Starlight/space-station-14/pull/227 por completo
+    // private FormattedMessage GetArmorPenetrationExplain(string proto)
+    // {
+    //     var msg = new FormattedMessage();
+    //     if (!ProtoManager.TryIndex<HitscanPrototype>(proto, out var entityProto))
+    //         return msg;
+
+    //     if (entityProto.ArmorPenetration == 0)
+    //         return msg;
+
+    //     if (entityProto.ArmorPenetration > 0)
+    //     {
+    //         msg.PushNewline();
+    //         msg.TryAddMarkup(Loc.GetString("damage-examine-penetration-positive", ("penetration", entityProto.ArmorPenetration * 100)), out var error);
+    //     }
+
+    //     if (entityProto.ArmorPenetration < 0)
+    //     {
+    //         msg.PushNewline();
+    //         msg.TryAddMarkup(Loc.GetString("damage-examine-penetration-negative", ("penetration", entityProto.ArmorPenetration * -100)), out var error);
+    //     }
+    //     return msg;
+    // }
+
+    private DamageSpecifier? GetHitscanProjectileDamage(string proto) {
+        if (!ProtoManager.TryIndex<HitscanPrototype>(proto, out var entityProto))
+            return null;
+
+        if (entityProto.Damage == null)
+            return null;
+
+        if (!entityProto.Damage.Empty)
+            return entityProto.Damage * Damageable.UniversalHitscanDamageModifier;
+
+        return null;
+    }
+
+    private void OnHitScanCartridgeExamine(EntityUid uid, HitScanCartridgeAmmoComponent component, ExaminedEvent args)
+    {
+        if (component.Spent)
+            args.PushMarkup(Loc.GetString("gun-cartridge-spent"));
+        else
+            args.PushMarkup(Loc.GetString("gun-cartridge-unspent"));
+    }
 }
