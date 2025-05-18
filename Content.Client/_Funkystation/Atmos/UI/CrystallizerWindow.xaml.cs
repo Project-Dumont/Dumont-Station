a60bb7f9e8b80d7584b76774dd0dd8a673bbe755
marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
2025-05-18 18:35:29 -0400

    crystallizer ui tweaks and cap input rate (https://github.com/funky-station/funky-station/pull/799)
    Co-authored-by: Steve <marlumpy@gmail.com>

--- Content.Client/_Funkystation/Atmos/UI/CrystallizerWindow.xaml.cs
+++ Content.Client/_Funkystation/Atmos/UI/CrystallizerWindow.xaml.cs
@@ -15,6 +15,7 @@ using Content.Shared.Atmos;
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
 using System.Numerics;
+using System.Linq;
 
 namespace Content.Client._Funkystation.Atmos.UI
 {
@@ -41,7 +42,7 @@ namespace Content.Client._Funkystation.Atmos.UI
             IoCManager.InjectDependencies(this);
             InitializeRecipeButtons();
 
-            GasInput.OnValueChanged += args => OnGasInputChanged?.Invoke(args.Value);
+            GasInput.OnValueChanged += args => OnGasInputChanged?.Invoke(Math.Clamp(args.Value, 0f, 250f));
         }
 
         public void SetActive(bool active)
@@ -149,6 +150,20 @@ namespace Content.Client._Funkystation.Atmos.UI
             RecipesContainer.Children.Clear();
             _buttonToRecipeId.Clear();
 
+            var orderedRecipeIds = new List<string>
+            {
+                "ammoniaCrystalRecipe",
+                "metalHydrogenRecipe",
+                "healiumCrystalRecipe",
+                "protoNitrateCrystalRecipe",
+                "supermatterSliverRecipe",
+                "nitrousOxideCrystalRecipe",
+                "diamondRecipe",
+                "plasmaSheetRecipe",
+                "crystalCellRecipe",
+                "zaukeriteRecipe"
+            };
+
             _nothingButton.Text = "Nothing";
             _nothingButton.HorizontalExpand = true;
             _nothingButton.Pressed = true;
@@ -163,21 +178,50 @@ namespace Content.Client._Funkystation.Atmos.UI
             _buttonToRecipeId[_nothingButton] = null;
             RecipesContainer.AddChild(_nothingButton);
 
-            foreach (var recipe in _prototypeManager.EnumeratePrototypes<CrystallizerRecipePrototype>())
-            {
-                var button = new Button
-                {
-                    Text = recipe.Name,
-                    HorizontalExpand = true
-                };
+            var allRecipes = _prototypeManager.EnumeratePrototypes<CrystallizerRecipePrototype>().ToList();
+            var processedRecipeIds = new HashSet<string>();
 
-                button.OnPressed += _ =>
+            foreach (var recipeId in orderedRecipeIds)
+            {
+                var recipe = allRecipes.FirstOrDefault(r => r.ID == recipeId);
+                if (recipe != null)
                 {
-                    SelectRecipeButton(button, recipe);
-                    OnRecipeButtonPressed?.Invoke(button, recipe.ID);
-                };
-                _buttonToRecipeId[button] = recipe.ID;
-                RecipesContainer.AddChild(button);
+                    var button = new Button
+                    {
+                        Text = recipe.Name,
+                        HorizontalExpand = true
+                    };
+
+                    button.OnPressed += _ =>
+                    {
+                        SelectRecipeButton(button, recipe);
+                        OnRecipeButtonPressed?.Invoke(button, recipe.ID);
+                    };
+                    _buttonToRecipeId[button] = recipe.ID;
+                    RecipesContainer.AddChild(button);
+                    processedRecipeIds.Add(recipe.ID);
+                }
+            }
+
+            foreach (var recipe in allRecipes)
+            {
+                if (!processedRecipeIds.Contains(recipe.ID))
+                {
+                    var button = new Button
+                    {
+                        Text = recipe.Name,
+                        HorizontalExpand = true
+                    };
+
+                    button.OnPressed += _ =>
+                    {
+                        SelectRecipeButton(button, recipe);
+                        OnRecipeButtonPressed?.Invoke(button, recipe.ID);
+                    };
+                    _buttonToRecipeId[button] = recipe.ID;
+                    RecipesContainer.AddChild(button);
+                    processedRecipeIds.Add(recipe.ID);
+                }
             }
         }
 
@@ -284,7 +328,7 @@ namespace Content.Client._Funkystation.Atmos.UI
                             HorizontalExpand = true
                         };
 
-                        var backgroundColor = (index % 2 == 0) ? Color.FromHex("#1B1B1E") : Color.FromHex("#2F2F38");
+                        var backgroundColor = (index % 2 == 0) ? Color.FromHex("#222222") : Color.FromHex("#2F2F38");
                         panel.PanelOverride = new StyleBoxFlat { BackgroundColor = backgroundColor };
 
                         GasList.AddChild(panel);
