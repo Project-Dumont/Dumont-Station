c562035e6c176b84c529ab82f4dbad47bb35b769
GabyChangelog <agentepanela2@gmail.com>
2025-05-22 20:05:22 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/StationEvents/Components/GameDirectorComponent.cs
+++ Content.Goobstation.Server/StationEvents/Components/GameDirectorComponent.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 AgentePanela <agentepanela@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aidenkrz <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 // SPDX-FileCopyrightText: 2025 misghast <51974455+misterghast@users.noreply.github.com>
--- Content.Server/_Funkystation/GameDirectors/SecretDirectorSystem.cs
+++ Content.Server/_Funkystation/GameDirectors/SecretDirectorSystem.cs
@@ -16,6 +16,7 @@
 // SPDX-FileCopyrightText: 2024 deltanedas <39013340+deltanedas@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 lzk <124214523+lzk228@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 misghast <51974455+misterghast@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
