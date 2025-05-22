funky-station/funky-station

Director tweaks (#535)
* Reduced changeling infection knockout time to 2 minutes. Increased the time it takes for symptoms to show by 5 minutes and with it the entire process to compensate for the decrease in time of sleep

* dynamic fixed 65% chance (#1714)

* game director hell

* fixed some shit

* finish yml

* fix runn in lobby and tags conflcit

* fixed

* wizards midround

* solid progress

* review time @durkle,@shitcode,@aviu,@mocho

* okay for real now take a look @whateverusername0 @gluesniffler @Aidenkrz :trollface:

* restored and fixed eventmanager thanks sx-7

* code cleanup

* door metrics was redundant

* chaosmetric cleanup

* added amag and modified emagcount by door access

* blood changes

* CCVars, thats spelled wrong

* fix typo

* charge metrics, event tables

* bigfix

* nuked unsed usings and dependancies

* fixed indentation

* more indentaisnf fixes

* restoration

* :trollface:

* fixed my test errors, now its just lavaland stuff pls get a testserver up as fast as possible :trollface:

* removed meteors entirely cause fuck that shit

* fix?

* fix.

* fixed antagroundstart not working on a live server

* bingle chaos

* lgtfm

* tellerrename

* the sleeper

* fixsleepr

* minplayers

* noroundstartantags for the sleeper

* backwards logic

* :trollface:

---------

Co-authored-by: Aidenkrz <aiden@djkraz.com>

(cherry picked from commit 6a3dbafb30e14bbc0e7325eb8469cdf27eb04aa9)

* oops they were actually important

* added jetpack tag which needed to be added for some reason??? maybe i accidentally removed during merge

* Hopefully fixed yaml errors

* tweaked numbers

* tweaked combat values #2

* BIG balance changes to game directors

* tweaked hostile creature combat value

---------

Co-authored-by: gus <august.eymann@gmail.com>

Content.Server/_Goobstation/StationEvents/Metric/Components/CombatMetricComponent.cs
@@ -7,11 +7,18 @@ namespace Content.Server._Goobstation.StationEvents.Metric.Components;
 [RegisterComponent, Access(typeof(CombatMetricSystem))]
 public sealed partial class CombatMetricComponent : Component
 {
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public FixedPoint2 HostileScore = 10.0f;
 
+    /// <summary>
+    /// Funky: The rough combat potential of a carp
+    /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public FixedPoint2 FriendlyScore = 10.0f;
+    public FixedPoint2 HostileScore = 5.0f;
+
+    /// <summary>
+    /// Funky: The rough combat potential of an (unrobust) friendly tider (was 10.0 with Goob)
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public FixedPoint2 FriendlyScore = 2.0f;
 
     /// <summary>
     ///   Cost per point of medical damage for friendly entities
@@ -23,13 +30,13 @@ public sealed partial class CombatMetricComponent : Component
     ///   Cost for friendlies who are in crit
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public FixedPoint2 CritScore = 10.0f;
+    public FixedPoint2 CritScore = 2.0f;
 
     /// <summary>
     ///   Cost for friendlies who are dead
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public FixedPoint2 DeadScore = 20.0f;
+    public FixedPoint2 DeadScore = 10.0f;
 
     [DataField, ViewVariables(VVAccess.ReadWrite)]
     public FixedPoint2 maxItemThreat = 15.0f;
@@ -41,17 +48,20 @@ public sealed partial class CombatMetricComponent : Component
     public Dictionary<string, FixedPoint2> ItemThreat =
         new()
         {
-            { "Taser", 2.0f },
-            { "Sidearm", 2.0f },
+            { "Taser", 3.0f },
+            { "Sidearm", 3.0f },
             { "Rifle", 5.0f },
-            { "HighRiskItem", 2.0f },
-            { "CombatKnife", 1.0f },
-            { "Knife", 1.0f },
+            { "HighRiskItem", 4.0f },
+            { "CombatKnife", 2.0f },
+            { "Knife", 1.5f },
             { "Grenade", 2.0f },
-            { "Bomb", 2.0f },
-            { "MagazinePistol", 0.5f },
+            { "Bomb", 4.0f },
+            { "MagazinePistol", 1.0f },
             { "Hacking", 1.0f },
             { "Jetpack", 1.0f },
+            { "Armor", 3.0f},
+            { "SpecialArmor", 6.0f},
+            { "SpecialWeapon", 6.0f},
         };
 
 }

Project-Dumont/Dumont-Station

Content.Goobstation.Server/StationEvents/Metric/Components/CombatMetricComponent.cs
@@ -5,18 +5,25 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Goobstation.Maths.FixedPoint;
+//using Content.Goobstation.Maths.FixedPoint;
 
 namespace Content.Goobstation.Server.StationEvents.Metric.Components;
 
 [RegisterComponent, Access(typeof(CombatMetricSystem))]
 public sealed partial class CombatMetricComponent : Component
 {
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public double HostileScore = 10.0f;
 
+    /// <summary>
+    /// Funky: The rough combat potential of a carp
+    /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public double FriendlyScore = 10.0f;
+    public double HostileScore = 5.0f;
+
+    /// <summary>
+    /// Funky: The rough combat potential of an (unrobust) friendly tider (was 10.0 with Goob)
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public double FriendlyScore = 2.0f;
 
     /// <summary>
     ///   Cost per point of medical damage for friendly entities
@@ -28,13 +35,13 @@ public sealed partial class CombatMetricComponent : Component
     ///   Cost for friendlies who are in crit
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public double CritScore = 10.0f;
+    public double CritScore = 2.0f;
 
     /// <summary>
     ///   Cost for friendlies who are dead
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public double DeadScore = 20.0f;
+    public double DeadScore = 10.0f;
 
     [DataField, ViewVariables(VVAccess.ReadWrite)]
     public double maxItemThreat = 15.0f;
@@ -46,17 +53,20 @@ public sealed partial class CombatMetricComponent : Component
     public Dictionary<string, double> ItemThreat =
         new()
         {
-            { "Taser", 2.0f },
-            { "Sidearm", 2.0f },
+            { "Taser", 3.0f },
+            { "Sidearm", 3.0f },
             { "Rifle", 5.0f },
-            { "HighRiskItem", 2.0f },
-            { "CombatKnife", 1.0f },
-            { "Knife", 1.0f },
+            { "HighRiskItem", 4.0f },
+            { "CombatKnife", 2.0f },
+            { "Knife", 1.5f },
             { "Grenade", 2.0f },
-            { "Bomb", 2.0f },
-            { "MagazinePistol", 0.5f },
+            { "Bomb", 4.0f },
+            { "MagazinePistol", 1.0f },
             { "Hacking", 1.0f },
             { "Jetpack", 1.0f },
+            { "Armor", 3.0f},
+            { "SpecialArmor", 6.0f},
+            { "SpecialWeapon", 6.0f},
         };
 
 }