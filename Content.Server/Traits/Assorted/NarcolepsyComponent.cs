d6fdaff10dd647e3b13306ab4f91f101be46ea1c
GabyChangelog <agentepanela2@gmail.com>
2025-05-30 14:12:01 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Traits/Assorted/NarcolepsyComponent.cs
+++ Content.Server/Traits/Assorted/NarcolepsyComponent.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Lgibb18 <65973111+Lgibb18@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
--- Content.Server/Traits/Assorted/NarcolepsySystem.cs
+++ Content.Server/Traits/Assorted/NarcolepsySystem.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2023 Vordenburg <114301317+Vordenburg@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Lgibb18 <65973111+Lgibb18@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 //
 // SPDX-License-Identifier: MIT
 
