48b990e33150bd6608fd6d1392b261d124ed3f51
GabyChangelog <agentepanela2@gmail.com>
2025-06-22 18:09:20 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Funkystation/BloodCult/EntitySystems/CultistSpellSystem.cs
+++ Content.Server/_Funkystation/BloodCult/EntitySystems/CultistSpellSystem.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Skye <57879983+Rainbeon@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 kbarkevich <24629810+kbarkevich@users.noreply.github.com>
 //
