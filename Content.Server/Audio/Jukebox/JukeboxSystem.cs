f24087ed3f85325866b93e5eb444eb7caaf7fa4e
GabyChangelog <agentepanela2@gmail.com>
2025-06-22 18:20:44 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Audio/Jukebox/JukeboxSystem.cs
+++ Content.Server/Audio/Jukebox/JukeboxSystem.cs
@@ -38,6 +38,9 @@
 // SPDX-FileCopyrightText: 2024 stellar-novas <stellar_novas@riseup.net>
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Fildrance <fildrance@gmail.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 mkanke-real <mikekanke@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
