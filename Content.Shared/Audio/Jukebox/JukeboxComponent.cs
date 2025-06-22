6ed267a5f35d01867122503c5daa4102c1ec683a
GabyChangelog <agentepanela2@gmail.com>
2025-06-22 18:22:11 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Audio/Jukebox/JukeboxComponent.cs
+++ Content.Shared/Audio/Jukebox/JukeboxComponent.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 iNVERTED <alextjorgensen@gmail.com>
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 mkanke-real <mikekanke@gmail.com>
 //
 // SPDX-License-Identifier: MIT
