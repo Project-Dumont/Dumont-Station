b5a172907360275ba49270d511be8fe125c8f0c3
GabyChangelog <agentepanela2@gmail.com>
2025-06-22 18:05:41 +0000

    chore: Automatically update REUSE headers

--- Content.Server/GameTicking/Rules/RevolutionaryRuleSystem.cs
+++ Content.Server/GameTicking/Rules/RevolutionaryRuleSystem.cs
@@ -26,6 +26,7 @@
 // SPDX-FileCopyrightText: 2025 CerberusWolfie <wb.johnb.willis@gmail.com>
 // SPDX-FileCopyrightText: 2025 Errant <35878406+Errant-4@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GMWQ <garethquaile@gmail.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Gareth Quaile <garethquaile@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 John Willis <143434770+CerberusWolfie@users.noreply.github.com>
--- Content.Shared/Revolutionary/SharedRevolutionarySystem.cs
+++ Content.Shared/Revolutionary/SharedRevolutionarySystem.cs
@@ -7,6 +7,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 nikthechampiongr <32041239+nikthechampiongr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Skye <57879983+Rainbeon@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
