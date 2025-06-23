49741ce27631e70351140f49520a3d2a8e43fe10
GabyChangelog <agentepanela2@gmail.com>
2025-06-23 17:30:57 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
+++ Content.Server/Shuttles/Systems/EmergencyShuttleSystem.cs
@@ -56,6 +56,7 @@
 // SPDX-FileCopyrightText: 2024 stellar-novas <stellar_novas@riseup.net>
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 ScarKy0 <106310278+ScarKy0@users.noreply.github.com>
