8cd54271ae38b662c04aea4a59ae95cbc2998db1
GabyChangelog <agentepanela2@gmail.com>
2025-07-07 22:59:34 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -98,6 +98,7 @@
 // SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 J <billsmith116@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
