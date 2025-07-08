b1c6a4c666c9a405c05f2f046696e62b49b2a406
GabyChangelog <agentepanela2@gmail.com>
2025-07-08 21:27:26 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Administration/Managers/BanManager.cs
+++ Content.Server/Administration/Managers/BanManager.cs
@@ -10,6 +10,7 @@
 // SPDX-FileCopyrightText: 2024 Pieter-Jan Briers <pieterjan.briers@gmail.com>
 // SPDX-FileCopyrightText: 2025 AgentePanela <agentepanela@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
 // SPDX-FileCopyrightText: 2025 Panela <107573283+AgentePanela@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
