41707335f1b57acb80ad247826f7727336c39dbb
Panela <107573283+AgentePanela@users.noreply.github.com>
2025-07-08 18:31:45 -0300

    Update Content.Server/Administration/Managers/BanManager.cs

--- Content.Server/Administration/Managers/BanManager.cs
+++ Content.Server/Administration/Managers/BanManager.cs
@@ -221,7 +221,7 @@ public sealed partial class BanManager : IBanManager, IPostInjectInit
         if (targetUsername is not null)
         {
             var id = await _db.GetServerBanAsync(null, target, null, null); // We only need to get with the username
-            SendServerBanWebhook(banDef, user, adminName, minutes, id?.Id);
+            SendServerBanWebhook(banDef, targetUsername, adminName, minutes, id?.Id);
         } // Gabystation - ban webhook end
 
         KickMatchingConnectedPlayers(banDef, "newly placed ban");
