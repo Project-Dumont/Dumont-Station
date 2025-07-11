7f65c609a49fc347f70d175d458ab6d860452d9c
GabyChangelog <agentepanela2@gmail.com>
2025-07-11 21:27:25 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Chat/Systems/ChatSystem.Emote.cs
+++ Content.Server/Chat/Systems/ChatSystem.Emote.cs
@@ -16,6 +16,7 @@
 // SPDX-FileCopyrightText: 2025 AgentePanela <agentepanela@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Conchelle <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
