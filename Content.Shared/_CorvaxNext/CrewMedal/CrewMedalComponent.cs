b2314e90404e78ada2b561cc0eef254c2f38bd84
GabyChangelog <agentepanela2@gmail.com>
2025-07-12 19:46:21 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_CorvaxNext/CrewMedal/CrewMedalComponent.cs
+++ Content.Shared/_CorvaxNext/CrewMedal/CrewMedalComponent.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kill_Me_I_Noobs <118206719+Vonsant@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
