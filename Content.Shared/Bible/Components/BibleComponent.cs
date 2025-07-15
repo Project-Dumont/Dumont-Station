45da9c3a75f10385b6fb1d0da6fa8614972fa6a1
GabyChangelog <agentepanela2@gmail.com>
2025-07-15 12:06:56 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Bible/Components/BibleComponent.cs
+++ Content.Shared/Bible/Components/BibleComponent.cs
@@ -6,6 +6,7 @@
 // SPDX-FileCopyrightText: 2023 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Dreykor <arguemeu@gmail.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Solstice <solsticeofthewinter@gmail.com>
 // SPDX-FileCopyrightText: 2025 TheBorzoiMustConsume <197824988+TheBorzoiMustConsume@users.noreply.github.com>
