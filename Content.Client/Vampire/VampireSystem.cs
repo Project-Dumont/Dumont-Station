8ab3fda34c0fbb5eb92a12de4987bb84be3131c8
Dreykor <Dreykor12@gmail.com>
2025-07-16 17:02:29 -0300

    correções de whitespace pt3

--- Content.Client/Vampire/VampireSystem.cs
+++ Content.Client/Vampire/VampireSystem.cs
@@ -26,18 +26,18 @@ public sealed class VampireSystem : EntitySystem
         SubscribeLocalEvent<VampireIconComponent, GetStatusIconsEvent>(GetVampireIcon);
         SubscribeLocalEvent<VampireAlertComponent, UpdateAlertSpriteEvent>(OnUpdateAlert);
     }
-    
+
     private void GetVampireIcon(EntityUid uid, VampireIconComponent component, ref GetStatusIconsEvent args)
     {
         var iconPrototype = _prototype.Index(component.StatusIcon);
         args.StatusIcons.Add(iconPrototype);
     }
-    
+
     private void OnUpdateAlert(EntityUid uid, VampireAlertComponent component, ref UpdateAlertSpriteEvent args)
     {
         if (args.Alert.ID != component.BloodAlert)
             return;
-        
+
         var sprite = args.SpriteViewEnt.Comp;
         var blood = Math.Clamp(component.BloodAmount, 0, 999);
         sprite.LayerSetState(VampireVisualLayers.Digit1, $"{(blood / 100) % 10}");
