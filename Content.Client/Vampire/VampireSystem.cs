34f512889a836b4670d07afad49894bc47075e0a
GabyChangelog <agentepanela2@gmail.com>
2025-07-16 20:04:32 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Vampire/VampireSystem.cs
+++ Content.Client/Vampire/VampireSystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Rinary <72972221+Rinary1@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Dreykor <Dreykor12@gmail.com>
 // SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
