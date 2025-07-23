2fdd93c8889df00c3f523cd5c9c2f4ca49de51f8
GabyChangelog <agentepanela2@gmail.com>
2025-07-23 18:15:51 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/Hailer/HailerComponent.cs
+++ Content.Goobstation.Shared/Hailer/HailerComponent.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Nikita Rαmses Abdoelrahman <ramses@starwolves.io>
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
