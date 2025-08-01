835284fe36a7b6f55175153b59f4cf295ceb157c
GabyChangelog <agentepanela2@gmail.com>
2025-08-01 03:08:51 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Humanoid/HumanoidVisualLayers.cs
+++ Content.Shared/Humanoid/HumanoidVisualLayers.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2024 Rouge2t7 <81053047+Sarahon@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 ScyronX <166930367+ScyronX@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kayzel <43700376+KayzelW@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Michelle Szinger <chell@alterae.online>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
