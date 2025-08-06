51882297e5a0eb290ad1d466ee04102567ad6bf9
Kyoth25f <kyoth25f@gmail.com>
2025-08-06 10:28:20 -0300

    add: Adiciona o PVS override

--- Content.Shared/_Gabystation/StationTeleporter/SharedStationTeleporterSystem.UI.cs
+++ Content.Shared/_Gabystation/StationTeleporter/SharedStationTeleporterSystem.UI.cs
@@ -9,21 +9,33 @@ using Content.Shared._Gabystation.StationTeleporter.Components;
 using Content.Shared.Teleportation.Components;
 using Robust.Shared.Map;
 using Robust.Shared.Containers;
+using Robust.Shared.GameStates;
+using Robust.Shared.Player;
+using System.Collections;
+using Robust.Shared.Utility;
 
 namespace Content.Shared._Gabystation.StationTeleporter;
 
 public abstract partial class SharedStationTeleporterSystem
 {
-
     public readonly LocId UnknownNameLoc = "teleporter-name-unknown";
     public readonly LocId RdFirstPortalLoc = "teleporter-name-rd-first";
     public readonly LocId RdSecondPortalLoc = "teleporter-name-rd-second";
 
+    [Dependency] private readonly ISharedPlayerManager _players = default!;
+    [Dependency] private readonly SharedPvsOverrideSystem _pvsOverrideSystem = default!;
+
+    private Dictionary<EntityUid, HashSet<EntityUid>> _overridedEntities = new();
+
     // TODO Adicionar caso em que a entidade é deletada;
+    // TODO Ao remover um chip com o console aberto, o pvs override precisa ser removido;
+    // Abre ui, adiciona todos os overrides da lista, remove um chip, remove um teleporter da lista, fecha a ui, remove todos os overrider da lista. No final, o cliente ainda fica com o overrider do chip removido.
 
     private void InitializeUI()
     {
         SubscribeLocalEvent<StationTeleporterConsoleComponent, BoundUIOpenedEvent>(OnUIOpened);
+        SubscribeLocalEvent<StationTeleporterConsoleComponent, BoundUIClosedEvent>(OnUIClosed);
+
         SubscribeLocalEvent<StationTeleporterConsoleComponent, StationTeleporterClickMessage>(OnUIPortalClicked);
 
         SubscribeLocalEvent<StationTeleporterConsoleComponent, EntRemovedFromContainerMessage>(OnRemove);
@@ -32,7 +44,44 @@ public abstract partial class SharedStationTeleporterSystem
 
     private void OnUIOpened(Entity<StationTeleporterConsoleComponent> ent, ref BoundUIOpenedEvent args)
     {
+        if (_players.TryGetSessionByEntity(args.Actor, out var session))
+        {
+            foreach (var teleporter in ent.Comp.Teleporters)
+            {
+                _pvsOverrideSystem.AddSessionOverride(teleporter, session);
+                _overridedEntities.GetOrNew(args.Actor).Add(teleporter);
+            }
+
+            foreach (var handTeleporter in ent.Comp.HandTeleporters)
+            {
+                if (!TryComp<HandTeleporterComponent>(handTeleporter, out var handTeleporterComp))
+                    return;
+
+                if (handTeleporterComp.FirstPortal is { } firstPortalUid && EntityManager.EntityExists(handTeleporterComp.FirstPortal))
+                {
+                    _pvsOverrideSystem.AddSessionOverride(firstPortalUid, session);
+                    _overridedEntities.GetOrNew(args.Actor).Add(firstPortalUid);
+                }
+
+                if (handTeleporterComp.SecondPortal is { } secondPortalUid && EntityManager.EntityExists(handTeleporterComp.SecondPortal))
+                {
+                    _pvsOverrideSystem.AddSessionOverride(secondPortalUid, session);
+                    _overridedEntities.GetOrNew(args.Actor).Add(secondPortalUid);
+                }
+            }
+        }
+
         UpdateUserInterface(ent);
+    }
+
+    private void OnUIClosed(Entity<StationTeleporterConsoleComponent> _, ref BoundUIClosedEvent args)
+    {
+        if (!_players.TryGetSessionByEntity(args.Actor, out var session)
+            || !_overridedEntities.TryGetValue(args.Actor, out var entities))
+            return;
+
+        foreach (var ent in entities)
+            _pvsOverrideSystem.RemoveSessionOverride(ent, session);
     }
 
     private void OnUIPortalClicked(Entity<StationTeleporterConsoleComponent> ent,
