31353d421f5f2162b65c4cb4129c929b4f788146
GabyChangelog <agentepanela2@gmail.com>
2025-09-24 21:33:17 +0000

    chore: Automatically update REUSE headers

--- Content.Client/MalfAI/MalfAiViewportWindow.xaml.cs
+++ Content.Client/MalfAI/MalfAiViewportWindow.xaml.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tyranex <bobthezombie4@gmail.com>
 // SPDX-FileCopyrightText: 2025 funkystationbot <funky@funkystation.org>
 //
