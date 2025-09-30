3e5e9b01689e0a2f84a9abd2adf6e61d01f8300d
Kyoth25f <kyoth25f@gmail.com>
2025-09-30 00:36:22 -0300

    tweak: networka `NanoBankCardComponent`

--- Content.Shared/_Gabystation/NanoBank/NanoBankCardComponent.cs
+++ Content.Shared/_Gabystation/NanoBank/NanoBankCardComponent.cs
@@ -3,30 +3,28 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-using Content.Shared._DV.CartridgeLoader.Cartridges;
 using Robust.Shared.GameStates;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared._Gabystation.NanoBank;
 
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class NanoBankCardComponent : Component
 {
-    [DataField]
+    [DataField, AutoNetworkedField]
     public bool LoggedIn = false;
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public int AccountId = 0;
 
     [DataField]
     public int AccountPin = 0;
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public bool NotificationsMuted = false;
 
     /// <summary>
     /// The station linked to the account.
     /// </summary>
-    [DataField]
+    [DataField, AutoNetworkedField]
     public EntityUid? Station;
 }
