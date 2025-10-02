8481c7054fa2957bf5bbe8fa2f23dcd457dead3e
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2025-10-03 03:32:56 +1300

    Remove static IoC from client & server EntryPoint (#40562)
    * Remove static IoC from client & server EntryPoint
    
    * Fix missing log manager initialization
    
    * file namespace

--- Content.Shared/Entry/EntryPoint.cs
+++ Content.Shared/Entry/EntryPoint.cs
@@ -64,7 +64,7 @@ namespace Content.Shared.Entry
         public override void PreInit()
         {
             IoCManager.InjectDependencies(this);
-            SharedContentIoC.Register();
+
             VerifyModules(); // Goobstation - Module Throws
         }
 
