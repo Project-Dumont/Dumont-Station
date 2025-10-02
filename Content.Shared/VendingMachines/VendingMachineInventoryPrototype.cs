8d4f9d02c2658db4de953b86f9d28c457d9a2cdd
GabyChangelog <agentepanela2@gmail.com>
2025-10-02 23:13:06 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/VendingMachines/VendingMachineInventoryPrototype.cs
+++ Content.Shared/VendingMachines/VendingMachineInventoryPrototype.cs
@@ -15,6 +15,7 @@
 // SPDX-FileCopyrightText: 2025 AgentePanela <agentepanela@gmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
+// SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
 // SPDX-FileCopyrightText: 2025 Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
 // SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
