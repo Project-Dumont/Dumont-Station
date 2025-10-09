532e0c7525be81848a5b328cf5f5c052c746881b
GabyChangelog <agentepanela2@gmail.com>
2025-10-09 19:55:58 +0000

    chore: Automatically update REUSE headers

--- Content.Server/MalfAI/MalfAiDoomsdaySystem.cs
+++ Content.Server/MalfAI/MalfAiDoomsdaySystem.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Dreykor <arguemeu@gmail.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tyranex <bobthezombie4@gmail.com>
 // SPDX-FileCopyrightText: 2025 funkystationbot <funky@funkystation.org>
