df4ecd1c6ae89e1e149b908838af562304b8a9a6
BombasterDS2 <bombasterds.github@mail.ru>
2025-10-11 04:11:12 +1000

    ChargesVisualLayers & RevPropaganda fix (#20)
    * ChargesVisualLayers & RevPropaganda charges logic fix
    
    * whaat
    
    * ?????
    
    ---------
    
    Co-authored-by: BombasterDS <deniskaporoshok@gmail.com>

--- Content.Shared/_EinsteinEngines/Revolutionary/RevolutionaryConverterSystem.cs
+++ Content.Shared/_EinsteinEngines/Revolutionary/RevolutionaryConverterSystem.cs
@@ -10,8 +10,11 @@ using Content.Shared.Chat;
 using Content.Shared.Dataset;
 using Content.Shared.DoAfter;
 using Content.Shared.Flash;
+using Content.Shared.Humanoid;
 using Content.Shared.Interaction;
 using Content.Shared.Interaction.Events;
+using Content.Shared.Mind;
+using Content.Shared.Mind.Components;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Random.Helpers;
 using Content.Shared.Revolutionary.Components;
@@ -89,6 +92,7 @@ public sealed class RevolutionaryConverterSystem : EntitySystem
             || args.Target == null)
             return;
 
+        _chargesSystem.TryUseCharges(entity.Owner, entity.Comp.ConsumesCharges);
         ConvertTarget(args.Used.Value, args.Target.Value, args.User);
     }
 
@@ -101,25 +105,18 @@ public sealed class RevolutionaryConverterSystem : EntitySystem
 
     public void OnConverterAfterInteract(Entity<RevolutionaryConverterComponent> entity, ref AfterInteractEvent args)
     {
-        if (entity.Comp.ApplyFlashEffect) // Traumastation
-            return;
-
         if (args.Handled
             || !args.Target.HasValue
             || !args.CanReach
-            || (entity.Comp.ConsumesCharges > 0
-            && !_chargesSystem.TryUseCharges(entity.Owner, entity.Comp.ConsumesCharges)))
+            || !_chargesSystem.HasCharges(entity.Owner, entity.Comp.ConsumesCharges)
+            || !HasComp<MindContainerComponent>(args.Target)
+            || !HasComp<HumanoidAppearanceComponent>(args.Target)) // TraumaStation change
             return;
 
-        /* Trauma edit
+        // TraumaStation
         if (entity.Comp.ApplyFlashEffect)
-        {
-            _flash.Flash(args.Target.Value, args.User, entity.Owner, entity.Comp.FlashDuration, entity.Comp.SlowToOnFlashed, melee: true);
+            _flash.Flash(args.Target.Value, args.User, entity.Owner, entity.Comp.FlashDuration, entity.Comp.SlowToOnFlashed);
 
-            bool hasChargesLeft = entity.Comp.ConsumesCharges <= 0 || _chargesSystem.HasCharges(entity.Owner, entity.Comp.ConsumesCharges);
-            _appearance.SetData(entity.Owner, FlashVisuals.Flashing, hasChargesLeft);
-            _appearance.SetData(entity.Owner, FlashVisuals.Burnt, !hasChargesLeft);
-        } */
 
         if (args.Target is not { Valid: true } target
             || !HasComp<MobStateComponent>(target)
