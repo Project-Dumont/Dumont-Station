f23f83e41ae493bb812c3feba731df3de9fe2ea7
GabyChangelog <agentepanela2@gmail.com>
2025-10-12 18:11:24 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Fluids/SharedPuddleSystem.Spillable.cs
+++ Content.Shared/Fluids/SharedPuddleSystem.Spillable.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
+// SPDX-FileCopyrightText: 2025 Will-Oliver-Br <164823659+Will-Oliver-Br@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
