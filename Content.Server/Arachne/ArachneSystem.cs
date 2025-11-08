222eb3781cd391b667844122b5d50bf79836f646
GabyChangelog <agentepanela2@gmail.com>
2025-11-08 19:03:38 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Arachne/ArachneSystem.cs
+++ Content.Server/Arachne/ArachneSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Crono209ggg <crono209gg@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Arachne;
 using Content.Shared.Actions;
 using Content.Shared.IdentityManagement;
