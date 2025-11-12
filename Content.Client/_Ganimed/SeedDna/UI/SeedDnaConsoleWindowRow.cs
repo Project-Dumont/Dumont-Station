3d0d2ec036c9747208778b349ef4d1ee34779323
GabyChangelog <agentepanela2@gmail.com>
2025-11-12 18:47:14 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_Ganimed/SeedDna/UI/SeedDnaConsoleWindowRow.cs
+++ Content.Client/_Ganimed/SeedDna/UI/SeedDnaConsoleWindowRow.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 CrimeMoot <wakeafa@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._Ganimed.SeedDna;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
--- Content.Server/Botany/SeedPrototype.cs
+++ Content.Server/Botany/SeedPrototype.cs
@@ -13,10 +13,13 @@
 // SPDX-FileCopyrightText: 2023 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 PraxisMapper <praxismapper@gmail.com>
-// SPDX-FileCopyrightText: 2024 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Token <esil.bektay@yandex.com>
 // SPDX-FileCopyrightText: 2024 drakewill-CRL <46307022+drakewill-CRL@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 CrimeMoot <wakeafa@gmail.com>
+// SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
