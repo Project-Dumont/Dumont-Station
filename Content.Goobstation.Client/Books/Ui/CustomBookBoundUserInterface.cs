ad674c9646682e31c570baf1698d765a7a4be7fd
GabyChangelog <agentepanela2@gmail.com>
2025-11-16 19:18:45 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Client/Books/Ui/CustomBookBoundUserInterface.cs
+++ Content.Goobstation.Client/Books/Ui/CustomBookBoundUserInterface.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 FaDeOkno <logkedr18@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Shared.Books;
 using JetBrains.Annotations;
 using Robust.Client.UserInterface;
