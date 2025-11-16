de74924887a46bc12dc3dccebabd924f9c82c3a1
GabyChangelog <agentepanela2@gmail.com>
2025-11-16 19:08:08 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Administration/Systems/GoobAdminVerbSystem.Antag.cs
+++ Content.Goobstation.Server/Administration/Systems/GoobAdminVerbSystem.Antag.cs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 LuciferMkshelter <154002422+LuciferEOS@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
