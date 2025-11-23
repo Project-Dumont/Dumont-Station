e93026e905e61ab1aca6fedacf06938f9283f50f
GabyChangelog <agentepanela2@gmail.com>
2025-11-23 19:41:51 +0000

    chore: Automatically update REUSE headers

--- Content.Client/DisplacementMap/DisplacementMapSystem.cs
+++ Content.Client/DisplacementMap/DisplacementMapSystem.cs
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Ed <96445749+TheShuEd@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Hannah Giovanna Dawson <karakkaraz@gmail.com>
 // SPDX-FileCopyrightText: 2025 Tayrtahn <tayrtahn@gmail.com>
 //
