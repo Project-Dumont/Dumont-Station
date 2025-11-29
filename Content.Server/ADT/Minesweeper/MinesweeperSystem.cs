c15aebaf0826db1e467c1a1e1b9a06782d50c382
GabyChangelog <agentepanela2@gmail.com>
2025-11-29 15:29:15 +0000

    chore: Automatically update REUSE headers

--- Content.Server/ADT/Minesweeper/MinesweeperSystem.cs
+++ Content.Server/ADT/Minesweeper/MinesweeperSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Explosion.EntitySystems;
 using Content.Shared.ADT.Minesweeper;
 using Content.Shared.Emag.Systems;
