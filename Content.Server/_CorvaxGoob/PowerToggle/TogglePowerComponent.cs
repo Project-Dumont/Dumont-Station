ea1738fea747554323b24e14741853e1987aada1
GabyChangelog <agentepanela2@gmail.com>
2025-11-30 16:53:51 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_CorvaxGoob/PowerToggle/TogglePowerComponent.cs
+++ Content.Server/_CorvaxGoob/PowerToggle/TogglePowerComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._CorvaxGoob.PowerToggle;
 using Robust.Shared.Audio;
 
