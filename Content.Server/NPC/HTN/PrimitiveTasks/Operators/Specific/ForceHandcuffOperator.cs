5609f1d7304f57c1456a916fcbbc587fa3bca1b6
GabyChangelog <agentepanela2@gmail.com>
2025-12-01 20:49:10 +0000

    chore: Automatically update REUSE headers

--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/ForceHandcuffOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/ForceHandcuffOperator.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Cuffs;
 using Content.Shared.Cuffs.Components;
 using Robust.Shared.Audio;
--- Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/PickNearbyWantedOperator.cs
+++ Content.Server/NPC/HTN/PrimitiveTasks/Operators/Specific/PickNearbyWantedOperator.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Administration.Logs.Converters;
 using Content.Server.NPC.Pathfinding;
 using Content.Shared.Cuffs.Components;
