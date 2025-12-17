b9907b80af4adebd94113dbb839281de9723f546
GabyChangelog <agentepanela2@gmail.com>
2025-12-17 17:45:37 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/Chat/RadioIconsEvents/RadioIconEvents.cs
+++ Content.Shared/Chat/RadioIconsEvents/RadioIconEvents.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Inventory;
 using Content.Shared.StatusIcon;
 using Robust.Shared.Prototypes;
