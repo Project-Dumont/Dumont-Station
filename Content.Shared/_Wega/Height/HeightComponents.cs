e1edf1001e45c16ca967c8557c610eff74234bb0
Will-Oliver-Br <164823659+Will-Oliver-Br@users.noreply.github.com>
2025-12-22 13:29:56 -0300

    integra parcialmente com o sistema atual de altura

--- Content.Shared/_Wega/Height/HeightComponents.cs
+++ Content.Shared/_Wega/Height/HeightComponents.cs
@@ -1,14 +1,18 @@
+using System.Numerics;
 using Robust.Shared.GameStates;
 
-namespace Content.Shared.Height
-{
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class SmallHeightComponent : Component
-    {
-    }
+namespace Content.Shared.Height;
 
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class BigHeightComponent : Component
-    {
-    }
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SmallHeightComponent : Component
+{
+    [DataField]
+    public Vector2? OriginalScale;
+}
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class BigHeightComponent : Component
+{
+    [DataField]
+    public Vector2? OriginalScale;
 }
