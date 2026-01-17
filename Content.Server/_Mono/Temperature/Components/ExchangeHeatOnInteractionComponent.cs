b6b43f1d2fa6c2cb8b8c7eaf3316fa0cafb5a83e
NazrinNya <137837419+NazrinNya@users.noreply.github.com>
2026-01-18 01:35:42 +0300

    i did not do that.

--- Content.Server/_Mono/Temperature/Components/ExchangeHeatOnInteractionComponent.cs
+++ Content.Server/_Mono/Temperature/Components/ExchangeHeatOnInteractionComponent.cs
@@ -8,9 +8,6 @@ public sealed partial class ExchangeHeatOnInteractionComponent : Component
 {
     /// <summary>
     /// Number that will be multiplied on temperature delta
-    /// = 1 means that temperatures will swap.
-    /// less than 1 means that temperatures will exchange their energy.
-    /// less than 0 or more than 1 means that you are a retard
     /// </summary>
     [DataField]
     public float Coefficient = 0.25f;
