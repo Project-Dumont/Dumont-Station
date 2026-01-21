73073944f8e5477a37635c2e497c2c35fd057488
GabyChangelog <agentepanela2@gmail.com>
2026-01-21 03:45:38 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Gabystation/ServerCurrency/Ghost/GhostSkinSystem.cs
+++ Content.Server/_Gabystation/ServerCurrency/Ghost/GhostSkinSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server._Gabystation.ServerCurrency.Managers;
 using Content.Server.Preferences.Managers;
 using Content.Shared._Gabystation.ServerCurrency.Ghost;
