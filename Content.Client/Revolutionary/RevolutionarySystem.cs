18ace9ff26f44bc50ad3d3dedacbb3bd011f16dd
GabyChangelog <agentepanela2@gmail.com>
2026-03-27 22:52:19 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Revolutionary/RevolutionarySystem.cs
+++ Content.Client/Revolutionary/RevolutionarySystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Revolutionary.Components;
 using Content.Shared.Revolutionary;
 using Content.Shared.StatusIcon.Components;
