cosmatic-drift-14/cosmatic-drift

Add Allergies (#649)

Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -18,6 +18,7 @@ using Robust.Shared.Utility;
 
 // CD: Imports
 using Content.Shared._CD.Records;
+using Content.Shared.FixedPoint;
 
 namespace Content.Shared.Preferences
 {
@@ -136,6 +137,9 @@ namespace Content.Shared.Preferences
         [DataField("cosmaticDriftCustomSpeciesName")]
         public string? CDCustomSpeciesName = null;
 
+        [DataField("cosmaticDriftAllergies")]
+        public Dictionary<string, FixedPoint2> CDAllergies = new();
+
         public HumanoidCharacterProfile(
             string name,
             string flavortext,
@@ -152,7 +156,8 @@ namespace Content.Shared.Preferences
             HashSet<ProtoId<TraitPrototype>> traitPreferences,
             Dictionary<string, RoleLoadout> loadouts,
             PlayerProvidedCharacterRecords? cdCharacterRecords,
-            string? cdCustomSpeciesName)
+            string? cdCustomSpeciesName,
+            Dictionary<string, FixedPoint2> cdAllergies)
         {
             Name = name;
             FlavorText = flavortext;
@@ -170,6 +175,7 @@ namespace Content.Shared.Preferences
             _loadouts = loadouts;
             CDCharacterRecords = cdCharacterRecords;
             CDCustomSpeciesName = cdCustomSpeciesName;
+            CDAllergies = cdAllergies;
 
             var hasHighPrority = false;
             foreach (var (key, value) in _jobPriorities)
@@ -203,7 +209,8 @@ namespace Content.Shared.Preferences
                 new HashSet<ProtoId<TraitPrototype>>(other.TraitPreferences),
                 new Dictionary<string, RoleLoadout>(other.Loadouts),
                 other.CDCharacterRecords,
-                other.CDCustomSpeciesName)
+                other.CDCustomSpeciesName,
+                other.CDAllergies)
         {
         }
 
@@ -488,6 +495,11 @@ namespace Content.Shared.Preferences
             return new HumanoidCharacterProfile(this) { CDCustomSpeciesName = customSpeciesName };
         }
 
+        public HumanoidCharacterProfile WithCDAllergies(Dictionary<string, FixedPoint2> allergies)
+        {
+            return new HumanoidCharacterProfile(this) { CDAllergies = allergies };
+        }
+
         public string Summary =>
             Loc.GetString(
                 "humanoid-character-profile-summary",
@@ -515,6 +527,7 @@ namespace Content.Shared.Preferences
             if (CDCharacterRecords != null && other.CDCharacterRecords != null &&
                 !CDCharacterRecords.MemberwiseEquals(other.CDCharacterRecords)) return false;
             if (CDCustomSpeciesName != other.CDCustomSpeciesName) return false;
+            if (!CDAllergies.SequenceEqual(other.CDAllergies)) return false;
             return Appearance.MemberwiseEquals(other.Appearance);
         }
 

Project-Dumont/Dumont-Station

Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -76,6 +76,7 @@ using Robust.Shared.Random;
 using Robust.Shared.Serialization;
 using Robust.Shared.Utility;
 using Content.Shared._CD.Records; // CD - Character Records
+using Content.Goobstation.Maths.FixedPoint;
 
 namespace Content.Shared.Preferences
 {
@@ -204,6 +205,9 @@ namespace Content.Shared.Preferences
         public PlayerProvidedCharacterRecords? CDCharacterRecords;
         // End CD - Character records
 
+        [DataField("cosmaticDriftAllergies")]
+        public Dictionary<string, FixedPoint2> CDAllergies = new();
+
         public HumanoidCharacterProfile(
             string name,
             string flavortext,
@@ -222,11 +226,10 @@ namespace Content.Shared.Preferences
             HashSet<ProtoId<TraitPrototype>> traitPreferences,
             Dictionary<string, RoleLoadout> loadouts,
             ProtoId<BarkPrototype> barkVoice, // Goob Station - Barks
-            // Begin CD - Character Records
-            PlayerProvidedCharacterRecords? cdCharacterRecords
-            // End CD - Character Records
-        )
-
+            // Begin CD - Character Records and Allergies
+            PlayerProvidedCharacterRecords? cdCharacterRecords,
+            Dictionary<string, FixedPoint2> cdAllergies)
+            // End CD - Character Records and Allergies
         {
             Name = name;
             FlavorText = flavortext;
@@ -245,9 +248,10 @@ namespace Content.Shared.Preferences
             _traitPreferences = traitPreferences;
             _loadouts = loadouts;
             BarkVoice = barkVoice; // Goob Station - Barks
-            // Begin CD - Character Records
+            // Begin CD - Character Records and Allergies
             CDCharacterRecords = cdCharacterRecords;
-            // End CD - Character Records
+            CDAllergies = cdAllergies;
+            // End CD - Character Records and Allergies
 
             var hasHighPrority = false;
             foreach (var (key, value) in _jobPriorities)
@@ -283,7 +287,10 @@ namespace Content.Shared.Preferences
                 new HashSet<ProtoId<TraitPrototype>>(other.TraitPreferences),
                 new Dictionary<string, RoleLoadout>(other.Loadouts),
                 other.BarkVoice, // Goob Station - Barks
-                other.CDCharacterRecords) // CD - Character Records
+                // Begin CD - Character Records and Allergies
+                other.CDCharacterRecords,
+                other.CDAllergies)
+                // End CD - Character Records and Allergies
         {
         }
 
@@ -611,6 +618,13 @@ namespace Content.Shared.Preferences
             };
         }
 
+        // Begin CD - Allergies
+        public HumanoidCharacterProfile WithCDAllergies(Dictionary<string, FixedPoint2> allergies)
+        {
+            return new HumanoidCharacterProfile(this) { CDAllergies = allergies };
+        }
+        // End CD - Allergies
+
         public string Summary =>
             Loc.GetString(
                 "humanoid-character-profile-summary",
@@ -638,8 +652,11 @@ namespace Content.Shared.Preferences
             if (!_traitPreferences.SequenceEqual(other._traitPreferences)) return false;
             if (!Loadouts.SequenceEqual(other.Loadouts)) return false;
             if (FlavorText != other.FlavorText) return false;
-            if (CDCharacterRecords != null && other.CDCharacterRecords != null && // CD
-                !CDCharacterRecords.MemberwiseEquals(other.CDCharacterRecords)) return false; // CD
+            // Begin CD - Character Records and Allergies
+            if (CDCharacterRecords != null && other.CDCharacterRecords != null &&
+                !CDCharacterRecords.MemberwiseEquals(other.CDCharacterRecords)) return false;
+            if (!CDAllergies.SequenceEqual(other.CDAllergies)) return false;
+            // End CD - Character Records and Allergies
             return Appearance.MemberwiseEquals(other.Appearance);
         }
 