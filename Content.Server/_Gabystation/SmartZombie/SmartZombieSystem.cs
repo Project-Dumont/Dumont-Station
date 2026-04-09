50cc5dd2d2be399aa8c27198302b88b3425199f4
GabyChangelog <agentepanela2@gmail.com>
2026-04-09 15:50:23 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Gabystation/SmartZombie/SmartZombieSystem.cs
+++ Content.Server/_Gabystation/SmartZombie/SmartZombieSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Zombies;
 using Content.Shared._Gabystation.SmartZombie;
 using Content.Shared.Weapons.Melee;
--- Content.Shared/_Gabystation/SmartZombie/SmartZombieComponent.cs
+++ Content.Shared/_Gabystation/SmartZombie/SmartZombieComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 namespace Content.Shared._Gabystation.SmartZombie;
 /// <summary>
 /// Anti-fun police, here's your fucking knob to make this component way less FUN!
