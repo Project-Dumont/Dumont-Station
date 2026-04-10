cc35804e71d75908683642a82eafe65b718a1602
TheRealHeartbeat <enzoprado784@gmail.com>
2026-04-10 14:06:25 -0300

    funcionando, agora preciso adicionar mais objetivos e adicionar eles pros trabalhos

--- Content.Goobstation.Server/MisandryBox/JobObjective/AddObjectiveSpecial.cs
+++ Content.Goobstation.Server/MisandryBox/JobObjective/AddObjectiveSpecial.cs
@@ -1,18 +1,37 @@
-﻿using Content.Shared.Roles;
+﻿using System.Linq;
+using Content.Shared.Roles;
+using Robust.Shared.Random;
 
 namespace Content.Goobstation.Server.MisandryBox.JobObjective;
 
 public sealed partial class AddObjectiveSpecial : JobSpecial
 {
     /// <summary>
-    /// List of objective prototypes to assign to this job
+    /// List of objective prototypes to randomly assign to this job
     /// </summary>
     [DataField("objectives", required: true)]
     public List<string> Objectives = new();
 
+    /// <summary>
+    /// Number of objectives to randomly select from the list. Defaults to 1.
+    /// If greater than available objectives, will select all objectives.
+    /// </summary>
+    [DataField("count")]
+    public int Count = 1;
+
     public override void AfterEquip(EntityUid mob)
     {
+        if (Objectives.Count == 0)
+            return;
+
         var system = IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<JobObjectiveSystem>();
-        system.QueueObjectives(mob, Objectives);
+        var random = IoCManager.Resolve<IRobustRandom>();
+        
+        var count = Math.Min(Count, Objectives.Count);
+        var shuffled = new List<string>(Objectives);
+        random.Shuffle(shuffled);
+        var selectedObjectives = shuffled.Take(count).ToList();
+        
+        system.QueueObjectives(mob, selectedObjectives);
     }
 }
--- Resources/Locale/pt-BR/_Gabystation/objectives/conditions/steal-target-groups.ftl
+++ Resources/Locale/pt-BR/_Gabystation/objectives/conditions/steal-target-groups.ftl
@@ -1,2 +1,3 @@
 steal-target-groups-proto-rcd = Proto RCD
 steal-target-groups-rosethorn-blade = Lâmina da Rosa
+steal-target-groups-medal = Uma medalha
