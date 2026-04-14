3a5170edf6e89b307ca321d528db59da443ca8ba
GabyChangelog <agentepanela2@gmail.com>
2026-04-14 16:16:40 +0000

    chore: Automatically update REUSE headers

--- Content.Trauma.Client/IoC/ContentTraumaClientIoC.cs
+++ Content.Trauma.Client/IoC/ContentTraumaClientIoC.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Trauma.Client.Humanoid;
 
 namespace Content.Trauma.Client.IoC;
