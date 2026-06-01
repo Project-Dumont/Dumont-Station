57ec3557b0aecb7570bdf28c6c7f32206deb3259
Will-Oliver-Br <164823659+Will-Oliver-Br@users.noreply.github.com>
2026-06-01 18:21:58 -0300

    sem sair do grab em softcritical

--- Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
+++ Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
@@ -157,6 +157,7 @@ public sealed class PullingSystem : EntitySystem
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SharedVirtualItemSystem _virtualSystem = default!;
     [Dependency] private readonly SharedCombatModeSystem _combatMode = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!; // Orion
 
     public override void Initialize()
     {
@@ -738,6 +739,9 @@ public sealed class PullingSystem : EntitySystem
 
         if (pullerUidNull == null)
             return true;
+
+        if (user == pullableUid && _mobState.IsSoftCritical(pullableUid)) // Orion
+            return false;
 
         var msg = new AttemptStopPullingEvent(user);
         RaiseLocalEvent(pullableUid, ref msg, true);
