53247643caa6bc9154bf082afe8f22c9d324a2b0
Dumont Station <dumontstation@gmail.com>
2026-06-20 02:01:23 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Orion/Bitrunning/Systems/NetpodSystem.cs
+++ Content.Server/_Orion/Bitrunning/Systems/NetpodSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Popups;
 using Content.Server._Orion.Bitrunning.Components;
 using Content.Server.Power.Components;
--- Content.Server/_Orion/CloningAppearance/Systems/CloningAppranceSystem.cs
+++ Content.Server/_Orion/CloningAppearance/Systems/CloningAppranceSystem.cs
@@ -1,4 +1,8 @@
-﻿using Content.Server._Orion.CloningAppearance.Components;
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Server._Orion.CloningAppearance.Components;
 using Content.Server._Orion.CloningAppearance.Events;
 using Content.Server.Clothing.Systems;
 using Content.Server.GameTicking;
