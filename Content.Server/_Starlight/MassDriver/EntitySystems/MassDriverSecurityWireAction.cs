c62eea9eb731635b0b84237837e46052b0b4c503
Dumont Station <dumontstation@gmail.com>
2026-07-02 16:03:15 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Starlight/MassDriver/EntitySystems/MassDriverSecurityWireAction.cs
+++ Content.Server/_Starlight/MassDriver/EntitySystems/MassDriverSecurityWireAction.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Wires;
 using Content.Shared._Starlight.MassDriver;
 using Content.Shared._Starlight.MassDriver.Components;
