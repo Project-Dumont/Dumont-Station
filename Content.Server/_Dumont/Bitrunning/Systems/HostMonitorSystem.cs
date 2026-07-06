58522ccc8f865e24c3890439a2fb366863cb62b6
Dumont Station <dumontstation@gmail.com>
2026-07-06 02:04:13 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Dumont/Bitrunning/Systems/HostMonitorSystem.cs
+++ Content.Server/_Dumont/Bitrunning/Systems/HostMonitorSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared._Orion.Bitrunning;
 using Content.Shared._Orion.Bitrunning.Components;
 using Content.Shared.Damage;
