974cdd039854474dc6c4115be8cc596eaaf39a9d
Dumont Station <dumontstation@gmail.com>
2026-07-07 03:41:42 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Canvas/Ui/CanvasBoundUserInterface.cs
+++ Content.Client/Canvas/Ui/CanvasBoundUserInterface.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Client.UserInterface;
 using Content.Shared.Canvas;
 using Robust.Client.GameObjects;
