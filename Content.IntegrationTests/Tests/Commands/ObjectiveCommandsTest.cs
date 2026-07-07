6b2446341b2d77360bdae4c2cb436bfb56e37334
Dumont Station <dumontstation@gmail.com>
2026-07-07 19:40:10 +0000

    chore: Automatically update REUSE headers

--- Content.IntegrationTests/Tests/Commands/ObjectiveCommandsTest.cs
+++ Content.IntegrationTests/Tests/Commands/ObjectiveCommandsTest.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 #nullable enable
 using System.Linq;
 using Content.Server.Objectives;
