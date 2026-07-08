231200ab6c25830dcf66f2aafca52b27c07ce109
Eduardo <104707805+isTheSuperN0va@users.noreply.github.com>
2026-07-08 09:16:59 -0300

    unshitcode 1

--- Content.Server/_Impstation/CartridgeLoader/Cartridges/SOSCartridgeSystem.cs
+++ Content.Server/_Impstation/CartridgeLoader/Cartridges/SOSCartridgeSystem.cs
@@ -9,6 +9,7 @@ using Content.Shared.PDA;
 using Content.Shared.Popups;
 using Robust.Server.Containers;
 using Robust.Shared.Containers;
+using Robust.Shared.Timing;
 
 namespace Content.Server._Impstation.CartridgeLoader.Cartridges;
 
@@ -17,6 +18,7 @@ public sealed class SOSCartridgeSystem : EntitySystem
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
     [Dependency] private readonly RadioSystem _radio = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
     {
@@ -25,58 +27,40 @@ public sealed class SOSCartridgeSystem : EntitySystem
         SubscribeLocalEvent<SOSCartridgeComponent, CartridgeActivatedEvent>(OnActivated);
     }
 
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        var query = EntityQueryEnumerator<SOSCartridgeComponent>();
-        while (query.MoveNext(out var uid, out var comp))
-        {
-            if (comp.Timer > 0)
-            {
-                comp.Timer -= frameTime;
-            }
-        }
-    }
-
     private void OnActivated(EntityUid uid, SOSCartridgeComponent component, CartridgeActivatedEvent args)
     {
-        if (component.CanCall)
-        {
-            //Get the PDA
-            if (!TryComp<PdaComponent>(args.Loader, out var pda))
-                return;
+        if (!HasComp<PdaComponent>(args.Loader))
+            return;
 
-            //Get the id container
-            if (_container.TryGetContainer(args.Loader, SOSCartridgeComponent.PDAIdContainer, out var idContainer))
+        if (component.NextMinimumTime < _timing.CurTime &&
+            _container.TryGetContainer(args.Loader, SOSCartridgeComponent.PDAIdContainer, out var idContainer)) {
+
+            //If theres nothing in id slot, send message anonymously
+            if (idContainer.ContainedEntities.Count == 0)
+                _radio.SendRadioMessage(uid, component.LocalizedDefaultName + " " + component.LocalizedHelpMessage, component.HelpChannel, uid);
+            else
             {
-                //If theres nothing in id slot, send message anonymously
-                if (idContainer.ContainedEntities.Count == 0)
+                //Otherwise, send a message with the full name of every id in there
+                foreach (var idCard in idContainer.ContainedEntities)
                 {
-                    _radio.SendRadioMessage(uid, component.LocalizedDefaultName + " " + component.LocalizedHelpMessage, component.HelpChannel, uid);
-                }
-                else
-                {
-                    //Otherwise, send a message with the full name of every id in there
-                    foreach (var idCard in idContainer.ContainedEntities)
-                    {
-                        if (!TryComp<IdCardComponent>(idCard, out var idCardComp))
-                            return;
+                    if (!TryComp<IdCardComponent>(idCard, out var idCardComp))
+                        continue;
 
-                        _radio.SendRadioMessage(uid, idCardComp.FullName + " " + component.LocalizedHelpMessage, component.HelpChannel, uid);
-                    }
+                    _radio.SendRadioMessage(uid, (idCardComp.FullName ?? component.LocalizedDefaultName) + " " + component.LocalizedHelpMessage, component.HelpChannel, uid);
                 }
-
-                component.Timer = SOSCartridgeComponent.TimeOut;
-                // DeltaV - send feedback that you succeeded
-                _popupSystem.PopupEntity(Loc.GetString("sos-message-sent-success"), uid, PopupType.Medium);
             }
+
+            // have to do this bullshit cuz timespan can't be constant
+            component.NextMinimumTime = TimeSpan.FromSeconds(_timing.CurTime.TotalSeconds + SOSCartridgeComponent.TimeOut);
+            // DeltaV - send feedback that you succeeded
+            _popupSystem.PopupEntity(Loc.GetString("sos-message-sent-success"), uid, PopupType.Medium);
         }
-        // DeltaV - send feedback that you failed
-        else
-        {
-            var seconds = Math.Round(component.Timer);
+        else {
+            var seconds = component.NextMinimumTime.TotalSeconds - _timing.CurTime.TotalSeconds;
+            seconds = Math.Round(seconds);
             _popupSystem.PopupEntity(Loc.GetString("sos-message-sent-cooldown", ("count", seconds)), uid, PopupType.MediumCaution);
         }
     }
+
+
 }
