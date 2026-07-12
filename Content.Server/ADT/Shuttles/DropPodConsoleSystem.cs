33389066576b4fa9cdf247165d0ee43c9862dd83
Dumont Station <dumontstation@gmail.com>
2026-07-12 19:39:12 +0000

    chore: Automatically update REUSE headers

--- Content.Server/ADT/Shuttles/DropPodConsoleSystem.cs
+++ Content.Server/ADT/Shuttles/DropPodConsoleSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Chat.Systems;
 using Content.Server.Decals;
 using Content.Server.GameTicking.Rules.Components;
