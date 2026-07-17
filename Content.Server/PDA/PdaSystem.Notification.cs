1cba5051e60270fa6197e22e1c9043a92e4a221d
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2026-07-17 00:26:35 -0300

    + sistema de notificação (algo nesse rumo)

--- Content.Server/PDA/PdaSystem.Notification.cs
+++ Content.Server/PDA/PdaSystem.Notification.cs
@@ -8,10 +8,45 @@ namespace Content.Server.PDA
 {
     public sealed partial class PdaSystem : SharedPdaSystem
     {
+        [Dependency] private readonly IPrototypeManager _proto = default!;
 
         public void OnPdaNotification(Entity<PdaComponent> ent, ref PdaNotificationEvent args)
         {
+            if (ent.Comp.IdSlot.Item is not { } idCardUid)
+                return;
 
+            if (!TryComp<AccessComponent>(idCardUid, out var accessComp))
+                return;
+
+            var accessLevels = accessComp.Tags;
+
+            PdaNotifyByAccess(args.Group.Access, accessLevels, ent.Comp, args);
+            PdaNotifyByGroups(args.Group.AccessGroup, accessLevels, ent.Comp, args);
+        }
+
+        public void PdaNotifyByAccess(
+            IEnumerable access,
+            HashSet<ProtoId<AccessLevelPrototype>> accessLevels,
+            PdaComponent pdaComp,
+            PdaNotificationEvent args)
+        {
+            foreach (var accessSingular in access) {
+                if (accessLevels.Contains(accessSingular)) {
+                    pdaComp.Notifications,Add(args.Message);
+                    UpdateState();
+                }
+            }
+        }
+
+        public void PdaNotifyByGroups(
+            IEnumerable group,
+            HashSet<ProtoId<AccessLevelPrototype>> accessLevels,
+            PdaComponent pdaComp,
+            PdaNotificationEvent args)
+        ) {
+            foreach (var accessGroup in group) {
+                PdaNotifyByAccess(accessGroup.Tags, accessLevels, pdaComp, args);
+            }
         }
     }
 }
--- Content.Shared/PDA/PdaNotificationEvent.cs
+++ Content.Shared/PDA/PdaNotificationEvent.cs
@@ -6,12 +6,8 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared.PDA;
 
-public sealed partial class PdaNotificationEvent(string message, NotificationOptions options) : HandledEntityEventArgs {
+public sealed partial class PdaNotificationEvent(string message, ProtoId<NotificationGroupPrototype> group, bool isLoud = false) : HandledEntityEventArgs {
     public readonly string Message = message;
-    public readonly NotificationOptions Options = options;
-}
-
-public sealed class NotificationOptions(bool isLoud, ProtoId<NotificationGroupPrototype> group) {
-    public readonly bool IsLoud = isLoud;
-    public readonly ProtoId<NotificationGroupPrototype> Group = group;
+    public readonly string Group = group;
+    public readonly string IsLoud = isLoud;
 }
