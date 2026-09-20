376c20b347669d60fb248b7bc9bb5d3e0ca0509a
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2026-09-20 18:06:36 -0300

    add componente

---
+++ Content.Shared/_Dumont/Cartridge/JaniCartridgeComponent.cs
@@ -0,0 +1,31 @@
+using Content.Shared.PDA;
+using Robust.Shared.Prototypes;
+
+[RegisterComponent]
+public sealed partial class JaniCartridgeComponent : Component
+{
+    /// <summary>
+    /// Jani notification group
+    /// </summary>
+    [DataField]
+    public ProtoId<NotificationGroupPrototype> NotificationGroup = "Janitor";
+
+    /// <summary>
+    /// Localized Message that appears on Jani's PDAs;
+    /// </summary>
+    [DataField]
+    public LocId LocalizedMessage = "jani-cartridge-message";
+
+    /// <summary>
+    /// Determines the amount of time to wait between calling the Janitor.
+    /// Made in order to prevent abuse.
+    /// </summary>
+    [DataField]
+    public TimeSpan CallDelay = TimeSpan.FromSeconds(10);
+
+    /// <summary>
+    /// Last used time, used for delay;
+    /// </summary>
+    [DataField]
+    public TimeSpan LastUsed = TimeSpan.Zero;
+}
